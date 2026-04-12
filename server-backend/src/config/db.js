92a7fd3e6853b90a0f2dd2d465b02acb94bcd850
Robin <xi2f@hotmail.com>
2026-04-12 19:52:34 +0300

    Add local database conection

---
+++ server-backend/src/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connect to DB successfully");
+}
+
+module.exports = { connectDB };
