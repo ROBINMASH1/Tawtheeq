3edf4f1e14e3e854def7e3786a0b8f31e014a5c2
Robin <xi2f@hotmail.com>
2026-04-13 18:52:39 +0300

    Add configuration

--- server-backend/src/server.js
+++ server-backend/src/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const { connectDB } = require("./config/db");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require("dotenv").config();
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const authRoutes = require("./routes/auth");
-app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to the Tawtheeq API");
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/students", require("./routes/students"));
 
 async function startServer() {
   try {
+    await connectDB();
+
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
