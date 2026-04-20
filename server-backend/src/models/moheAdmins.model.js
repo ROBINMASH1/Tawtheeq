9a8e8ef02b1f36757b38e5ef5563d60c26db4c45
Robin <xi2f@hotmail.com>
2026-04-20 21:26:17 +0300

    Add EmployeeID for MoheAdmin

--- server-backend/src/models/moheAdmins.model.js
+++ server-backend/src/models/moheAdmins.model.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
-const moheAdminSchema = new mongoose.Schema({}, { timestamps: true });
+const moheAdminSchema = new mongoose.Schema(
+  {
+    EmployeeID: { type: String, required: true, unique: true },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("MoheAdmin", moheAdminSchema);
