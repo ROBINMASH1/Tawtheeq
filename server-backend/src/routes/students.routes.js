64ccf9a8061c8944c1db260e8a39f381e50ae21f
Robin <xi2f@hotmail.com>
2026-05-01 14:47:23 +0300

    Add forgetPassword routes

---
+++ server-backend/src/routes/students.routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const studentController = require("../controllers/student.controller");
+const { authMiddleware } = require("../middleware/jwtAuth");
+const { requireRole } = require("../middleware/requireRole");
+
+router.post("/create", authMiddleware, requireRole("uniUser"), studentController.createStudent);
+
+router.post("/request-activation-otp", authMiddleware, requireRole("Student"), studentController.requestActivationOTP);
+
+router.post("/verify-otp", authMiddleware, requireRole("Student"), studentController.verifyOTP);
+
+router.post("/send-otp-forgot-password", studentController.sendOTPForgotPassword);
+
+router.post("/verify-otp-forgot-password", studentController.verifyOTPForgotPassword);
+
+module.exports = router;
