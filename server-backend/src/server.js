91b387d0a7671f063fe11201388d89128444a981
Robin <xi2f@hotmail.com>
2026-05-11 20:18:26 +0300

    Add security headers

--- server-backend/src/server.js
+++ server-backend/src/server.js
@@ -10,6 +10,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const helmet = require("helmet");
+app.use(helmet());
+
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
