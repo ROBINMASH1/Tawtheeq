90490fbe615ce7ad3287c5fc660794ab9e9dcc63
Robin <xi2f@hotmail.com>
2026-06-16 13:36:12 +0300

    Edit upload size limit

--- server-backend/src/middleware/upload.js
+++ server-backend/src/middleware/upload.js
@@ -1,9 +1,9 @@
 const multer = require('multer');
 
-// Single certificate upload — PDF only, 10 MB limit
+// Single certificate upload — PDF only, 1 MB limit
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 1 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'application/pdf') cb(null, true);
     else cb(new Error('Only PDF files are allowed.'), false);
@@ -14,7 +14,7 @@ const upload = multer({
 // Futuer work : we can save the files in the disk instead of memory for memory efficiency
 const bulkUpload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB
+  limits: { fileSize: 300 * 1024 * 1024 }, // 200 MB
   fileFilter: (req, file, cb) => {
     if (file.fieldname === 'csv' && file.mimetype === 'text/csv') cb(null, true);
     else if (file.fieldname === 'zip' && (
